ea4d4a128926b00e3519378f680e51cd62ac32ac
DeTry <sniacyinaczej@gmail.com>
2024-03-30 13:30:37 +0100

    1. Aktualizacja boarda - do zrobienia ikony i refaktoryzacja renderowania boarda z nowymi wlasciwosciami

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -4,27 +4,37 @@ import style2 from '../../css/Board/word_block.module.css'
 import { useEffect, useState } from 'react'
 
 function Board() {
+    const [ boardData, setBoardData] = useState([]);
     const [ dragged, setDragged ] = useState(null);
 
-    const words = ["S", "W", "P", "Ź", "A", "Ż", "O", "icon", "icon", "icon"]
+    const initializeBoardData = () => {
+        return [
+            ['TW', '', '', 'DL', '', '', '', 'TW', '', '', '', 'DL', '', '', 'TW'],
+            ['', 'DW', '', '', '', 'TL', '', '', '', 'TL', '', '', '', 'DW', ''],
+            ['', '', 'DW', '', '', '', 'DL', '', 'DL', '', '', '', 'DW', '', ''],
+            ['DL', '', '', 'DW', '', '', '', 'DL', '', '', '', 'DW', '', '', 'DL'],
+            ['', '', '', '', 'DW', '', '', '', '', '', 'DW', '', '', '', ''],
+            ['', 'TL', '', '', '', 'TL', '', '', '', 'TL', '', '', '', 'TL', ''],
+            ['', '', 'DL', '', '', '', 'DL', '', 'DL', '', '', '', 'DL', '', ''],
+            ['TW', '', '', 'DL', '', '', '', 'DW', '', '', '', 'DL', '', '', 'TW'],
+            ['', '', 'DL', '', '', '', 'DL', '', 'DL', '', '', '', 'DL', '', ''],
+            ['', 'TL', '', '', '', 'TL', '', '', '', 'TL', '', '', '', 'TL', ''],
+            ['', '', '', '', 'DW', '', '', '', '', '', 'DW', '', '', '', ''],
+            ['DL', '', '', 'DW', '', '', '', 'DL', '', '', '', 'DW', '', '', 'DL'],
+            ['', '', 'DW', '', '', '', 'DL', '', 'DL', '', '', '', 'DW', '', ''],
+            ['', 'DW', '', '', '', 'TL', '', '', '', 'TL', '', '', '', 'DW', ''],
+            ['TW', '', '', 'DL', '', '', '', 'TW', '', '', '', 'DL', '', '', 'TW']
+        ];
+    } 
 
-    const boardData = [
-        ['TW', '', '', 'DL', '', '', '', 'TW', '', '', '', 'DL', '', '', 'TW'],
-        ['', 'DW', '', '', '', 'TL', '', '', '', 'TL', '', '', '', 'DW', ''],
-        ['', '', 'DW', '', '', '', 'DL', '', 'DL', '', '', '', 'DW', '', ''],
-        ['DL', '', '', 'DW', '', '', '', 'DL', '', '', '', 'DW', '', '', 'DL'],
-        ['', '', '', '', 'DW', '', '', '', '', '', 'DW', '', '', '', ''],
-        ['', 'TL', '', '', '', 'TL', '', '', '', 'TL', '', '', '', 'TL', ''],
-        ['', '', 'DL', '', '', '', 'DL', '', 'DL', '', '', '', 'DL', '', ''],
-        ['TW', '', '', 'DL', '', '', '', 'DW', '', '', '', 'DL', '', '', 'TW'],
-        ['', '', 'DL', '', '', '', 'DL', '', 'DL', '', '', '', 'DL', '', ''],
-        ['', 'TL', '', '', '', 'TL', '', '', '', 'TL', '', '', '', 'TL', ''],
-        ['', '', '', '', 'DW', '', '', '', '', '', 'DW', '', '', '', ''],
-        ['DL', '', '', 'DW', '', '', '', 'DL', '', '', '', 'DW', '', '', 'DL'],
-        ['', '', 'DW', '', '', '', 'DL', '', 'DL', '', '', '', 'DW', '', ''],
-        ['', 'DW', '', '', '', 'TL', '', '', '', 'TL', '', '', '', 'DW', ''],
-        ['TW', '', '', 'DL', '', '', '', 'TW', '', '', '', 'DL', '', '', 'TW']
-    ];
+    useEffect(() => {
+        setBoardData(initializeBoardData());
+    }, [])
+
+    useEffect(() => {
+        console.log(boardData)
+    }, [boardData])
+
     const handleDrop = (event) => {
         event.preventDefault();
 
@@ -102,6 +112,37 @@ function Board() {
     useEffect(() => {
         console.log(dragged)
     }, [dragged])
+    
+    const words = ["S", "W", "P", "Ź", "A", "Ż", "O"]
+    const letters = ["S", "W", "P", "Ź", "A", "Ż", "O"];
+    const icons = ["potwierdz", "odrzuc", "pasuj"]
+
+    const assignIcons = (word) => {
+        if (word === "potwierdz") {
+            return " fa-solid fa-square-check fa-2x"
+        } else if (word === "odrzuc") {
+            return " fa-solid fa-square-xmark"
+        } else if (word === "pasuj") {
+            return " fa-solid fa-rotate"
+        }
+        return null
+    } 
+
+    const checkWhetherIcon = (word, bool) => {
+        if (bool == true) {
+            if (word !== "potwierdz" && word !== "odrzuc" && word !== "pasuj") {
+                return true
+            }
+            return false
+            
+        } else if (bool == false) {
+            if (word !== "potwierdz" && word !== "odrzuc" && word !== "pasuj") {
+                return word
+            }
+            return null
+        }
+
+    }
 
     return(
         <>
@@ -109,18 +150,48 @@ function Board() {
                 {renderBoardTiles()}
             </div>
 
-            <div className={style2['words-block']}>
+            {/* <div className={style2['words-block']}>
             { words.map((word, index) => (
                 <div 
                     key={index} 
                     id={index} 
-                    className={style2["letter-style"]} 
-                    draggable={word != "icon" ? true : false } 
+                    className={`${style2["letter-style"]} ${assignIcons(word)}`} 
+                    draggable={checkWhetherIcon(word, true) ? true : false } 
                     onDragStart={(event) => handleDragStart(event)} >
                     
-                    <span className={style2["span-style"]}>{word}</span>
+                    <span className={style2["span-style"]}>{checkWhetherIcon(word, false)}</span>
                 </div>
             ))}
+            </div> */}
+
+            <div 
+                className = {style2['words-block']} 
+            >
+                {letters.map((letter, index) => (
+                    <div
+                        key = {index}
+                        id = {index}
+                        className = {style2["letter-style"]}
+                        draggable = { true }
+                        onDragStart = { (event) => handleDragStart(event) }
+                    >
+                        <span
+                            className = {style2["span-style"]}
+                            >
+                            {letter} 
+                        </span>
+                    </div>
+                ))}
+
+                {icons.map((icon, index) => (
+                    <div
+                        key = {index}
+                        id = {index}
+                        className = {`${style2["letter-style"]} fas fa-solid fa-square-check`}
+                    >
+
+                    </div>
+                ))}
             </div>
         </>
     )
