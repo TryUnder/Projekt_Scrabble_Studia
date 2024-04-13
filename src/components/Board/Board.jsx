47619917e9ed4a0668e52e8cbaaa63c5d4143e4e
DeTry <sniacyinaczej@gmail.com>
2024-04-13 18:58:41 +0200

    aktualizacja Board.jsx - dodanie inicjalizacji słownika liter

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -10,6 +10,7 @@ function Board() {
     const [ draggedMain, setDraggedMain ] = useState(null);
     const [ previousBoardElements, setPreviousBoardElements ] = useState([])
     const [ wordBlockLetters, setWordBlockLetters ] = useState([])
+    const [ letterMap, setLetterMep ] = useState(null)
 
     const initializeBoardData = () => {
         const boardLayout = [
@@ -45,12 +46,55 @@ function Board() {
         return initialWordBlockLetters
     }
 
-    useEffect(() => {
-        setBoardData(initializeBoardData());
-    }, [])
+    const initializeLetterMap = () => {
+        const myLetterMap = new Map(Object.entries({
+            'Blank': { count: 2, points: 0 },
+
+            'A': { count: 9, points: 1 },
+            'E': { count: 7, points: 1 },
+            'I': { count: 8, points: 1 },
+            'N': { count: 5, points: 1 },
+            'O': { count: 6, points: 1 },
+            'R': { count: 4, points: 1 },
+            'S': { count: 4, points: 1 },
+            'W': { count: 4, points: 1 },
+            'Z': { count: 5, points: 1 },
+
+            'C': { count: 3, points: 2 },
+            'D': { count: 3, points: 2 },
+            'K': { count: 3, points: 2 },
+            'L': { count: 3, points: 2 },
+            'M': { count: 3, points: 2 },
+            'P': { count: 3, points: 2 },
+            'T': { count: 3, points: 2 },
+            'Y': { count: 4, points: 2 },
+
+            'B': { count: 2, points: 3 },
+            'G': { count: 2, points: 3 },
+            'H': { count: 2, points: 3 },
+            'J': { count: 2, points: 3 },
+            'Ł': { count: 2, points: 3 },
+            'U': { count: 2, points: 3 },
+
+            'Ą': { count: 1, points: 5 },
+            'Ę': { count: 1, points: 5 },
+            'F': { count: 1, points: 5 },
+            'Ó': { count: 1, points: 5 },
+            'Ś': { count: 1, points: 5 },
+            'Ż': { count: 1, points: 5 },
+
+            'Ć': { count: 1, points: 6 },
+            'Ń': { count: 1, points: 7 },
+            'Ź': { count: 1, points: 9 },
+        }));
+
+        return myLetterMap;
+    }
 
     useEffect(() => {
-        setWordBlockLetters(initializeBlockLetters())
+        setBoardData(initializeBoardData());
+        setWordBlockLetters(initializeBlockLetters());
+        setLetterMep(initializeLetterMap());
     }, [])
 
     useEffect(() => {
@@ -68,6 +112,10 @@ function Board() {
     useEffect(() => {
         console.log("Dragged: ", dragged)
     }, [dragged])
+
+    useEffect(() => {
+        console.log("literMap: ", letterMap)
+    }, [letterMap])
 
     const modifyPreviousBoardElements = (x, y) => {
         const newBoardData = [...boardData]
