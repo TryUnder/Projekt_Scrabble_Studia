6edba2b7b32e89d5d24b38b6d20ff853a8174ee1
DeTry <sniacyinaczej@gmail.com>
2024-05-04 18:04:03 +0200

    update

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const fileURLToPath =  require('url');
 const usersRoutes = require('./routes/usersRoutes.js')
 const boardRoutes = require('./routes/boardRoutes.js')
 const cookieParser = require('cookie-parser')
+const socketIo = require('socket.io'); // Importujemy socket.io
+
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -20,8 +22,30 @@ app.get('*', (req, res) => {
     res.sendFile(indexFilePath)
 })
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`Aplikacja działa na http://localhost:${port}`);
 });
 
-//module.exports = server;
+// Połączenie z Socket.io
+const io = socketIo(server);
+
+// Obsługa połączeń Socket.io
+io.on('connection', (socket) => {
+    console.log('Nowe połączenie WebSocket.');
+
+    // Obsługa zdarzenia 'example'
+    socket.on('example', (data) => {
+        console.log('Otrzymano dane:', data);
+        // Tutaj możesz wykonać odpowiednie akcje w zależności od otrzymanych danych
+    });
+
+    // Obsługa rozłączenia klienta
+    socket.on('disconnect', () => {
+        console.log('Klient rozłączony');
+    });
+});
+
+app.get('*', (req, res) => {
+    const indexFilePath = path.resolve(__dirname, '../dist', 'index.html');
+    res.sendFile(indexFilePath);
+});
