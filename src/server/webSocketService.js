005b7dda027d6b0cbbefa643450913c47db1adff
DeTry <sniacyinaczej@gmail.com>
2024-05-14 20:56:11 +0200

    update

--- src/server/webSocketService.js
+++ src/server/webSocketService.js
@@ -85,7 +85,7 @@ const initializeWebSocket = (server) => {
             console.log("to Player: ", toPlayer)
             //console.log("Player Login: ", playerLogin)
             //io.emit('sendPointsToClient', { wordsSum, wordSum, playerLogin
-            io.emit('sendPointsToClient', { userPoints, arrayPointsMapJSON, toPlayer })
+            socket.broadcast.emit('sendPointsToClient', { userPoints, arrayPointsMapJSON, toPlayer })
         })
 
         socket.on('sendMessage', ({message, playerLogin}) => {
